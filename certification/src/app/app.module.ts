1ef15c027507e79d6877e4f01babfb5a57ee6109
Pierre Arquevaux <pierre.arquevaux@gmail.com>
2022-12-21 00:57:57 +0100

    User list & add

--- certification/src/app/app.module.ts
+++ certification/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeModule } from './home/home.module';
+import { UsersModule } from './users/users.module';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { HomeModule } from './home/home.module';
   imports: [
     BrowserModule,
     HomeModule,
+    UsersModule,
     AppRoutingModule,
     NgbModule
   ],
