8baa1d1f4aa5bd0eaad187fbd6e70ba8d01a1cac
Joatan Le-Lostec <lelostecj@hotmail.fr>
2022-12-21 11:12:06 +0100

    mise en place de message

--- certification/src/app/messages/components/add/add.component.ts
+++ certification/src/app/messages/components/add/add.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
+import { FormControl, FormGroup } from '@angular/forms';
+import { MessageService } from '../../service/message.service';
+
+
+
+
+
+const url = "http://localhost:8080/message/post"
+
+
+
 @Component({
+
   selector: 'app-add',
+
   templateUrl: './add.component.html',
+
   styleUrls: ['./add.component.css']
+
 })
+
 export class AddComponent {
+
+
+
+  public form = new FormGroup({
+
+    content :new FormControl(),
+
+
+  })
+
+
+
+  constructor(
+
+    private messageService : MessageService
+
+  ){}
+
+
+
+  public submitMessage(): void {
+
+    // console.log(this.form.value);
+
+    this.messageService.createMessage(url, this.form.value)
+
+  }
 
 }
